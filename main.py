10e4100dc58e31d2af7bfa1487e3e750801438c0
Oscar Alcantara <oukiar@gmail.com>
2019-03-06 01:03:14 -0600

    agregados primeros videos demostrativos

--- main.py
+++ main.py
@@ -28,63 +28,93 @@ class Karaotool(FloatLayout):
            
             with open(self.base_filename + '.kot') as f:    #open karaotool text file
                 self.content = f.readlines()
-           
-        
-        self.btn_stepline = Button(text='Iniciar !', 
-                                    #size_hint=(1,None)
-                                    on_press=self.nextline
-                                    )
-        self.add_widget(self.btn_stepline)
-        
-        
-        self.video = Video(source=self.source, state='play')
-        
-        self.add_widget(self.video)
-        
-        self.kar_english = Label(text='englishh :)', 
-                                    size_hint_y=None,
-                                    font_size=32 )
-                                    
         
         self.line = 0;
         
-        self.add_widget(self.kar_english)
         
-        self.start = time.time()
+        self.video = Video(source=self.source, state='play', allow_stretch=True, keep_ratio=False)
+        
+        self.video.bind(on_loaded=self.on_loaded)
+        
+        self.kar_english = Label(text='KARAOKELAND', 
+                                    size_hint_y=None,
+                                    font_size=32 )
+                                
+                                
+        self.add_widget(self.video)
+        self.add_widget(self.kar_english)    
         
         #si existe el archivo con los tiempos
-        if os.path.exists(self.base_filename + '.kos'):
-            self.fsteps = open(self.base_filename + '.kos')
+        if os.path.exists(self.base_filename + '.kos'):            
+                
+            self.kar_spanish = Label(text='KARAOKELAND ESP', 
+                                        size_hint_y=1,
+                                        font_size=32 ) 
+                                        
+            self.add_widget(self.kar_spanish)
+            
+            self.fsteps = open(self.base_filename + '.kos') #steps
             self.steps = self.fsteps.readlines()
-            Clock.schedule_interval(self.stepchecker, .1)
+            
+            try:
+                self.efsteps = open(self.base_filename + '.koe') #subtitulos en lenguaje traducido
+                self.esteps = self.efsteps.readlines()
+            except:
+                pass
+            
+            #Clock.schedule_interval(self.stepchecker, .1)
+            
+            
+            self.video.bind(position=self.on_position)
             
             self.cursteptime = self.steps[self.line]
             
         else:
+            
+            self.btn_stepline = Button(text='Iniciar !', 
+                                        #size_hint=(1,None),
+                                        on_press=self.nextline
+                                        )
+            self.add_widget(self.btn_stepline, index=len(self.children) )
+            
+            
             #open for steps creation
             self.fsteps = open(self.base_filename + '.kos', 'w+')  #karaotool steps
+            
+            
+    def on_loaded(self, w):
+        print("Iniciando video")
         
-    def stepchecker(self, dt):
+    def on_position(self, w, val):
         
-        elapsed = time.time() - self.start
-        
-        if elapsed > float(self.cursteptime):
+        if val > float(self.cursteptime):
 
+            try:
+                self.kar_english.text = self.content[self.line]
+                try:
+                    self.kar_spanish.text = self.esteps[self.line]
+                except:
+                    pass
+                
+                self.line += 1
+                
+                #print "Step: ", self.video.position
+                
+                self.cursteptime = self.steps[self.line]
+            except:
+                self.kar_english.text = "END"
+                self.kar_spanish.text = "FIN"
+                
+        
+    def nextline(self, w):
+        try:
+            #advance one line
             self.kar_english.text = self.content[self.line]
             self.line += 1
             
-            print "Step: ", elapsed
-            
-            self.cursteptime = self.steps[self.line]
-        
-    def nextline(self, w):
-        #advance one line
-        self.kar_english.text = self.content[self.line]
-        self.line += 1
-        
-        elapsed = time.time() - self.start
-        
-        self.fsteps.write(str(elapsed) + '\n')
+            self.fsteps.write(str(self.video.position) + '\n')
+        except:
+            self.kar_english.text = "FIN"
 
 class Karaolang(FloatLayout):
     pass
